c4ee0f7ded91e4c58bf7a1eb58aafda240299ba3
Andreas <andreas.fichtner@gmail.com>
2017-03-09 22:27:07 +0100

    subscribe to sensor events

--- app/src/main/java/android_easywakeup/retterdesapok/de/easywakeup/BackgroundService.java
+++ app/src/main/java/android_easywakeup/retterdesapok/de/easywakeup/BackgroundService.java
@@ -5,11 +5,23 @@ import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Binder;
 import android.os.IBinder;
 import android.os.SystemClock;
+import android.preference.PreferenceManager;
+import android.util.Log;
 
-public class BackgroundService extends Service {
+public class BackgroundService extends Service implements SensorEventListener {
+
+    private SensorManager mSensorManager;
+    private Sensor proximitySensor;
+    private Sensor motionSensor;
+    private Sensor lightSensor;
 
     @Override
     public IBinder onBind(Intent arg0) {
@@ -39,6 +51,37 @@ public class BackgroundService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        SharedPreferences defaultSharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        boolean checkProximity = defaultSharedPreferences.getBoolean("switch_proximity", false);
+        boolean checkLight = defaultSharedPreferences.getBoolean("switch_ambient", false);
+        boolean checkMotion = defaultSharedPreferences.getBoolean("switch_accelerometer", false);
+
+        if(checkProximity) {
+            proximitySensor = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+            mSensorManager.registerListener(this, proximitySensor, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+
+        if(checkLight) {
+            lightSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+            mSensorManager.registerListener(this, lightSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+
+        if(checkMotion) {
+            motionSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_MOTION_DETECT);
+            mSensorManager.registerListener(this, motionSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        Log.d("Sensor", event.toString());
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Ignore
     }
 
     public class LocalBinder extends Binder {
